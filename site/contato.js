5b64b46729ea1b955b23c113603d91e0d44ebb11
Guilherme dos Santos <114258353+guilhermecarvalhosantos@users.noreply.github.com>
2024-09-12 16:52:32 -0300

    Add files via upload

---
+++ site/contato.js
@@ -0,0 +1,25 @@
+function Enviar() {
+    var nome = document.getElementById('nomeId');
+
+    if (nome.value != '') {
+        alert('Obrigado Senhor(a) ' + nome.value + 'os seus dados foram encaminhados com sucesso! (Essa funcionalidade ainda não está pronta, mas entre em contato pelos outros canais.)');
+    }
+}function Enviar() {
+    var nome = document.getElementById('nomeId');
+
+    if (nome.value != '') {
+        alert('Obrigado Senhor(a) ' + nome.value + 'os seus dados foram encaminhados com sucesso! (Essa funcionalidade ainda não está pronta, mas entre em contato pelos outros canais.)');
+    }
+}function Enviar() {
+    var nome = document.getElementById('nomeId');
+
+    if (nome.value != '') {
+        alert('Obrigado Senhor(a) ' + nome.value + 'os seus dados foram encaminhados com sucesso! (Essa funcionalidade ainda não está pronta, mas entre em contato pelos outros canais.)');
+    }
+}function Enviar() {
+    var nome = document.getElementById('nomeId');
+
+    if (nome.value != '') {
+        alert('Obrigado Senhor(a) ' + nome.value + 'os seus dados foram encaminhados com sucesso! (Essa funcionalidade ainda não está pronta, mas entre em contato pelos outros canais.)');
+    }
+}
